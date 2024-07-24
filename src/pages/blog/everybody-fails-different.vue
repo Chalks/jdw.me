84d4956f40729566175e34b1c1d860a79a8274a0
Jonathan Walters <skychalk@gmail.com>
2024-07-24 13:42:46 -0400

    tmp

---
+++ src/pages/blog/everybody-fails-different.vue
@@ -0,0 +1,96 @@
+<script setup>
+seo({
+    title: 'Everybody Fails Different',
+    description: 'Failing isn\'t what I thought it was',
+    imagePath: '/blog/death-of-formcake.jpg',
+    imageType: 'image/jpeg',
+    imageWidth: 800,
+    imageHeight: 511,
+    type: 'article',
+    published: '2024-01-21',
+});
+
+// https://search.google.com/test/rich-results
+</script>
+
+<template>
+    <article class="prose prose-red mx-auto py-12 px-4">
+        <h1>Everybody Fails Different</h1>
+
+        <p>
+            A few weeks ago Formcake was shut down. It was a business that I and
+            three of my friends worked on for about four years. We failed.
+        </p>
+
+        <p>
+            I failed.
+        </p>
+
+        <p>
+            There are a litany of reasons why Formcake died... poor marketing,
+            lack of focused product niche, scammers abusing the platform (a
+            surprisingly pervasive problem that attracted attention from
+            government agencies across the world), lack of integrations, and a
+            lack of interest from its creators. This is not the first time I've
+            failed either, I have a long list of side projects over the past
+            decade that have all ended more or less ignominiously.
+        </p>
+
+        <p>
+            There have been books written about failure by people smarter than
+            me. There are therapists, friends, and communities dedicated to
+            understanding how to recover from failure. There are people who will
+            trumpet to the high heavens how they failed a hundred times before
+            they found success. We all know what failure is and how it shapes
+            us. Despite all this, whenever we are faced with failure, it always
+            feels uniquely terrible to us. Everybody fails different.
+        </p>
+
+        <figure class="lg:mt-0">
+            <img src="/blog/death-of-formcake.jpg" alt="I spy bears and cars" />
+            <figcaption>
+                The death of Formcake.
+                <a href="https://unsplash.com/@jetztabertempo" target="_blank">photographer</a>
+            </figcaption>
+        </figure>
+
+        <p>
+            I am an optimist. I will occasionally buy a lottery ticket because
+            there's a part of me that goes "ooh, but what if" and I give into
+            that. I am a realist. When I buy a lottery ticket I roll my eyes a
+            little bit and know that my odds of winning The Big Prize are
+            virtually nil, so I don't really spend more than a couple bucks on
+            it every few months or so. I have never won the lottery. I doubt
+            that I ever will.
+        </p>
+
+        <p>
+            Working as an employee at a startup I have equity in the business.
+            I still draw a comfortable salary, so my equity is very much in the
+            lottery ticket status for me… which I think is okay. If the startup
+            I work for goes crazy and eventually IPOs, great! If it doesn't
+            that's fine because the opportunity cost is relatively low for me as
+            I'm doing things I'm interested in that I wouldn't necessarily get
+            in a more traditional role/business. But treating my side projects
+            this way leads to failure.
+        </p>
+
+        <p>
+            why
+        </p>
+
+        <p>
+            It finally occurred to me that one of the reasons I continually fail
+            at these projects is because I treat them like a lottery ticket.
+            There are all these stories about "unicorn" startups and how
+            incredibly life changing it is for everyone involved. Every time
+            I've started a new project I've had a "oooh, but what if" moment and
+            dreamed about that project hitting it big.
+        </p>
+
+        <p>
+            I've reconsidered what my goals are when building a product outside
+            of my main job.
+        </p>
+    </article>
+</template>
